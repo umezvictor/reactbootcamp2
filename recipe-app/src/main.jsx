2f3211b3132f4afa26bea161912e8c660fcb1069
Victor Umezuruike <victor.umezuruike@toyingroupsystems.com>
2025-03-22 16:38:58 +0100

    added routing

--- recipe-app/src/main.jsx
+++ recipe-app/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App.jsx";
+import About from "./pages/About.jsx";
 
-createRoot(document.getElementById('root')).render(
+//added react router setup here
+//RouterProvider defines the routes/paths and the components it should render
+
+const router = createBrowserRouter(
+  //defines paths and component to render
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ recipe-app/src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="App">
+      <main className="main_container">
+        <h1>Welcome to our page</h1>
+      </main>
+    </div>
+  );
+}
